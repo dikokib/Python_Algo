8a58c472ba2009dde765930cc2a152745f6a60da
dikokib <kouking1969@gmail.com>
2023-05-30 15:07:58 +0900

    windowsからの最後のpush

--- Sort/koko.py
+++ Sort/koko.py
@@ -1,4 +1,32 @@
-import random
+def merge_sort(numbers: list[int]) -> list[int]:
+    if len(numbers) <= 1:
+        return numbers
+    center = len(numbers) // 2
+    left = numbers[:center]
+    right = numbers[center:]
 
-nums = [random.randint(0, 1000) for i in range(15)]
-print(type(nums))
+    #自分で自分を呼び出す再起関数とは？
+    # ┗終了条件を持つ
+    # ┗毎回状態を変え、終了条件に近づく
+    # ┗それ自身を再帰的に呼び出す
+    print('****************************')
+    print("left:",left)
+    print("right:",right)
+
+
+    merge_sort(left)
+    merge_sort(right)
+
+    print('マージ終わった')
+
+    numbers = left + right
+
+    print("最後",numbers)
+
+    return numbers
+
+
+if __name__ == '__main__':
+    nums = [6, 2, 9, 5, 7, 1, 4]
+    #nums = [random.randint(0, 1000) for i in range(5)]
+    print(merge_sort(nums))
--- Sort/merge-sort.py
+++ Sort/merge-sort.py
@@ -11,12 +11,9 @@ def merge_sort(numbers: list[int]) -> list[int]:
     # ┗終了条件を持つ
     # ┗毎回状態を変え、終了条件に近づく
     # ┗それ自身を再帰的に呼び出す
+
     merge_sort(left)
     merge_sort(right)
-
-    print(left)
-    print(len(left))
-    #print(type(right))
 
 
     i = j = k = 0
@@ -43,5 +40,8 @@ def merge_sort(numbers: list[int]) -> list[int]:
 
 
 if __name__ == '__main__':
-    nums = [random.randint(0, 1000) for i in range(10)]
+    nums = [6, 2, 9, 5, 7, 1, 4]
+    #nums = [random.randint(0, 1000) for i in range(5)]
     print(merge_sort(nums))
+
+#windowsからの最後のpushです
